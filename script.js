6f02082eeddd5e6e6d85474389def0603fb151ba
Shivendra137 <156521000+Shivendra137@users.noreply.github.com>
2024-04-04 21:35:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,19 +4,28 @@ let menu = document.querySelector(".menu");
 
 let hiddenDivChild = document.querySelector(".hiddenDivChild");
 let hiddenDiv= document.querySelector(".hiddenDiv");
+let image= document.querySelector(".image");
 
 let var2 = true ;
 
+
+
+
 menu.addEventListener("click" , () => {
+
 
 
     if ( var2 === true ){
         hiddenDiv.style.display ="block";
         hiddenDiv.style.display ="flex";
-        hiddenDivChild.style.display ="block";
+        
         
         hiddenDivChild.style.display ="flex";
+        
+        image.style.marginTop ="70px";
 
+
+        
         
 
 
@@ -27,8 +36,20 @@ menu.addEventListener("click" , () => {
     else {
         hiddenDiv.style.display ="none";
         hiddenDivChild.style.display ="none";
+        image.style.marginTop ="250px";
         var2 = true ;
     }
+}
 
 
-} );
+
+
+
+ );
+
+
+// if (window.matchMedia("(max-width : 375px )" ).matches) {
+    
+    
+//     hiddenSearchBar.style.display = "none";
+// }
